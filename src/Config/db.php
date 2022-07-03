7fc35bf8389c13a9c15c978885c20a9d1e4bdd1b
Sakuni99 <sakunihiru@gmail.com>
2022-07-03 17:48:41 +0530

    db connection

--- src/Config/db.php
+++ src/Config/db.php
@@ -1,12 +1,22 @@
 <?php
 
-// Connection
-$conn = mysqli_connect("localhost", "root", "", "phoenix_trust_bank");
+class Connector
+{
+    private $conn;
+    function __construct()
+    {
+        $this->conn = mysqli_connect("localhost", "root", "", "phoenix_trust_bank");
 
-// Check if connection is
-// Successful or not
-if (!$conn) {
-    die("Connection failed: "
-        . mysqli_connect_error());
+
+        if (!$this->conn) {
+            die("Connection failed: "
+                . mysqli_connect_error());
+        }
+        echo "Connected successfully";
+    }
+
+    public function getConnector()
+    {
+        return $this->conn;
+    }
 }
-echo "Connected successfully";
