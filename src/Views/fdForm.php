959901a7e23fb83aab2c28fe5e6ca1ee53ef6e58
Sakuni99 <sakunihiru@gmail.com>
2022-07-12 18:32:14 +0530

    fd form created

--- src/Views/fdForm.php
+++ src/Views/fdForm.php
@@ -4,6 +4,23 @@ include '../Config/db.php';
 include '../Models/fdFormModel.php';
 include '../Controllers/fdFormController.php';
 
+if (isset($_SESSION['error_message'])) {
+    echo '<p style="color:red; font-size:1.2rem; align-self:center">' . $_SESSION['error_message'] . '</p>';
+    unset($_SESSION['error_message']);
+}
+
+$controller = new moneyTransferController();
+$validity = $controller->checkId();
+
+?>
+
+<?php
+
+$isSuccess = $controller->submitFdAccountDetails();
+if ($isSuccess) {
+    header("Location: fdCreatedSuccessfully.php");
+}
+
 ?>
 
 
@@ -20,7 +37,7 @@ include '../Controllers/fdFormController.php';
 </head>
 
 <body style="background-color: rgb(0,0,205);display:flex;flex-direction:column; ">
-
+    <!-- <main-header></main-header> -->
     <div class="container border border-2 m-5 p-5 mx-auto bg-light " style="margin-top: 50px;">
         <h2>First, check customers' eligibility for applying a loan </h2>
         <form id="myForm" action="fdForm.php" method="post">
@@ -38,7 +55,7 @@ include '../Controllers/fdFormController.php';
 
     <div class="container border border-2 m-5 p-5 mx-auto bg-light " id="div3" hidden>
         <h2>FD Form</h2>
-        <form>
+        <form id="myForm1" action="fdForm.php" method="post">
             <div class="form-group mt-3 ">
                 <label for="inputFullName">Full Name</label>
                 <input type="text" class="form-control" id="inputFullName" placeholder="Full Name">
@@ -99,23 +116,24 @@ include '../Controllers/fdFormController.php';
 
             <div class="form-row mt-3">
                 <div class="form-group ">
-                    <label for="inputLoanType">Loan Type</label>
-                    <select id="inputLoanType" class="custom-select mr-sm-2">
-                        <option selected>Choose...</option>
-                        <option>6 months</option>
-                        <option>1 years</option>
-                        <option>3 years</option>
-
+                    <label for="inputFdType">Fd Type</label>
+                    <select id="inputFdType" name="inputFdType" class="custom-select mr-sm-2" required>
+                        <option value="" disabled selected>choose</option>
+                        <?php
+                        $fd_types = $controller->getFdTypes();
+                        foreach ($fd_types as $fd_type) : ?>
+                            <option value=<?php echo $fd_type['fd_type_id']; ?>><?php echo $fd_type['fd_type_name']; ?></option>
+                        <?php endforeach; ?>
                     </select>
                 </div>
                 <div class="input-group mb-3">
                     <div class="col-md-2">
-                        <label for="inputLoanAmount">Amount</label>
+                        <label for="inputFdAmount">Amount</label>
                     </div>
                     <div class="input-group-prepend">
                         <span class="input-group-text">Rs.</span>
                     </div>
-                    <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" id="inputLoanAmount">
+                    <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" id="inputFdAmount" name="inputFdAmount" required>
                     <div class="input-group-append">
                         <span class="input-group-text">.00</span>
                     </div>
@@ -124,17 +142,34 @@ include '../Controllers/fdFormController.php';
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="inputAccNo">Savings Account Number </label>
-                    <input type="text" class="form-control" id="inputAccNo" placeholder="758****" disabled>
+                    <input type="text" class="form-control" id="inputAccNo" name="inputAccNo" placeholder="<?php echo $controller->getAccountNumber() ?> " disabled>
+                    <input type="hidden" name="hidden" value="<?php echo $controller->getAccountNumber(); ?>">
                 </div>
             </div>
-            <button type="submit" class="btn btn-primary">Apply</button>
+            <button type="submit" value="submit" class="btn btn-primary" name="apply" id="apply">Apply</button>
+
         </form>
+
     </div>
+    <?php
+    if ($validity == true && isset($_POST["submit1"])) {
+
+        $array = $controller->autoFill();
+    } else {
+        if (isset($_POST["submit1"])) {
+            $_SESSION['error_message'] = "Invalid Input";
+            echo '<p style="color:#dc3545; font-size:2rem; align-self:center">' . $_SESSION['error_message'] . '</p>';
+            unset($_SESSION['error_message']);
+        }
+    }
+
+    ?>
+
 
 
     <script type="text/javascript">
         const application = document.getElementById("div3");
-
+        var passedArray = <?php echo json_encode($array); ?>;
         // function EnableDisableTextBox() {
         //     var chkYes = document.getElementById("TaxYes");
         //     var inputTaxNo = document.getElementById("inputTaxNo");
@@ -149,16 +184,50 @@ include '../Controllers/fdFormController.php';
             application.hidden = !application.hidden;
 
         };
+
+        function autoFill() {
+            var full_name = document.getElementById("inputFullName");
+            var nic = document.getElementById("inputNIC");
+            var birthday = document.getElementById("birthday");
+            var email = document.getElementById("inputEmail");
+            var mobile = document.getElementById("inputMobile");
+            var address = document.getElementById("address");
+
+            // var amount = document.getElementById("inputLoanAmount");
+            // var sav_acc_no = document.getElementById("inputAccNo");
+            // var fd_no = document.getElementById("inputFDNo");
+
+            full_name.value = passedArray["full_name"];
+            nic.value = passedArray["nic"];
+            birthday.value = passedArray["birthday"];
+            email.value = passedArray["email"];
+            mobile.value = passedArray["contact_number"];
+            address.value = passedArray["address"];
+
+            // amount.value = passedArray["amount"];
+            // sav_acc_no.value = passedArray["sav_acc_no"];
+            // fd_no.value = passedArray["fd_no"];
+
+        };
+
+        // var form = document.getElementById("myForm1");
+
+        // function handleForm(event) {
+        //     event.preventDefault();
+        // }
+        // form.addEventListener('submit', handleForm);
     </script>
 
     <?php
     if (isset($_POST["submit1"])) {
-        $controller = new moneyTransferController();
-        $data = $controller->submitSavingAccount();
-        $account_number = $data['account_no'];
-        echo $account_number;
+
+        // $details = $controller->autoFill($account_number);
+        // echo $account_number;
+        //echo $details['full_name'];
         echo '<script type="text/javascript">showApplication();</script>';
+        echo '<script type="text/javascript"> autoFill();</script>';
     }
+
 
 
 
