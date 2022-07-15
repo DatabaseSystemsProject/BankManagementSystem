732d4c3302aa83336a335e40ff61e61e655d2f2c
Thiniraxz <thiniragenuka@gmail.com>
2022-07-16 01:21:16 +0530

    Add script for refreshing

--- src/Views/withdrawMoneyForm.php
+++ src/Views/withdrawMoneyForm.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-
-
 include 'base.php';
 include '../Controllers/withdrawController.php';
 $withdrawContr = new WithdrawController();
@@ -21,6 +19,11 @@ $myUrl = strval($account_type) . "Dashboard.php";
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Withdraw Money</title>
+    <script>
+        if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+        }
+    </script>
 </head>
 
 <body style="background-color: rgb(0,0,205);">
